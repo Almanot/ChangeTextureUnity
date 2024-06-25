9a7cf6c43436905aa7383501b473509d77a0a09c
Oleksii-Kupris <alexkupris1995@gmail.com>
2024-06-25 20:09:39 +0300

    added project

--- Assets/Scripts/ChangeTexture.cs
+++ Assets/Scripts/ChangeTexture.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class ChangeTexture : MonoBehaviour
 {
    public List<Texture> allTextures;
+   public GameObject ChangeTextureObject { get; set; }
    
-   public void ChangeTextureInObject(GameObject changeTextureObject, string textureName)    // Передаємо обєкт на якому будемо заміняти текстуру і саму назву текстури
+   // Я не знаю чи можна передавати GameObject з javascript сюди в метод. Тому якщо не можна то просто визивайте метод ChnageTextureWithOutObject() 
+ 
+   public void ChangeTextureInObject(string textureName, GameObject changeTextureObject)    // Передаємо обєкт на якому будемо заміняти текстуру і саму назву текстури
    {
       Texture currentTexture = allTextures.Find(x => x.name == textureName);  // Для простого тесту текстуру будемо брати з ліста по імені.
       Renderer renderer = changeTextureObject.GetComponent<Renderer>();
@@ -21,5 +24,21 @@ public class ChangeTexture : MonoBehaviour
          renderer.material.color = Color.red;
       }
    }
-   
+
+   public void ChnageTextureWithOutObject(string textureName)
+   {
+      Texture currentTexture = allTextures.Find(x => x.name == textureName);  
+      Renderer renderer = ChangeTextureObject.GetComponent<Renderer>();
+      
+      if (currentTexture && renderer)   
+      {
+         renderer.material.color = Color.white;
+         renderer.material.mainTexture = currentTexture;
+      }
+      else                                    
+      {
+         renderer.material.color = Color.red;
+      }
+   }
+
 }
